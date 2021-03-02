92e34b1bf6559b04bc553a61a3d7ad00ed82eca6
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-03-02 20:13:17 +0100

    - Added - account router to routes middleware.

--- server/middlewares/routes.js
+++ server/middlewares/routes.js
@@ -1,11 +1,16 @@
 import path from "path";
+
 import appConfigs from "./../configs/app.js";
 
+import accountRouter from "../routers/account.js";
 function fallback(req, res){
     res.sendFile(path.join(appConfigs.staticFilesPath, "index.html"));
 }
 
 export default function (app){
+    app.use("/account", accountRouter);
+
     //TODO: Add routers here.
+
     app.use(/.*/,fallback);
 }
