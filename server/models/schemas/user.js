3fb60435cf22f5a2a17c3d69f6b392aa7dc0fa0e
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-03-05 14:26:20 +0100

    - Added - user model schema.

--- server/models/schemas/user.js
+++ server/models/schemas/user.js
@@ -1,7 +1,34 @@
 import {Schema} from "mongoose";
+import {usernameLength} from "../../validations/account.js";
 
 const userSchema = new Schema({
-
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: usernameLength.min,
+        maxlength: usernameLength.max
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNumber: {
+        type: String
+    },
+    roles: {
+        type: Array,
+        required: true
+    },
+    passwordSalt: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    }
 });
 
 export default userSchema;
