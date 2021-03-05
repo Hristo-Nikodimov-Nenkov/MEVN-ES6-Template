6ac5d30eb6f556befcbc5f5ec22d6d96cefa457a
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-03-05 13:45:35 +0100

    - Added - cookieParser to app pipeline.

--- server/app.js
+++ server/app.js
@@ -3,14 +3,19 @@ import express from "express";
 import cors from "./middlewares/cors.js";
 import staticFiles from "./middlewares/staticFiles.js";
 import bodyParser from "./middlewares/bodyParser.js";
+import cookieParser from "./middlewares/cookieParser.js";
+
 import routes from "./middlewares/routes.js";
+
 import server from "./middlewares/server.js";
 
 const app = express();
-
 cors(app);
+
 staticFiles(app);
 bodyParser(app);
+
+cookieParser(app);
 
 routes(app);
 server(app);
