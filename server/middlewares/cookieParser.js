8e7faabb7397f3454d76a533a5cf4f0229501747
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-03-05 22:11:19 +0100

    - Split - cookieParser middleware to cookieParser and authenticate.

--- server/middlewares/cookieParser.js
+++ server/middlewares/cookieParser.js
@@ -1,8 +1,6 @@
 import cookieParser from "cookie-parser";
-import {authenticate} from "../services/security.js";
 import configs from "../configs/cookies.js";
 
 export default function (app) {
     app.use(cookieParser(configs.secret, configs.options));
-    app.use(authenticate());
 }
