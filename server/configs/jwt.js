f3ecaa15b0df782b2d1aa033da57dd529d51fb31
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2021-03-06 14:10:33 +0100

    - Added - jwt to configs.

---
+++ server/configs/jwt.js
@@ -0,0 +1,25 @@
+import appConfigs from "./app";
+
+export const defaultEnvironment = "development";
+
+const configs = {
+    "development": {
+        secret: "My JWT signing secret.",
+        options: {
+            issuer: `http://localhost:${appConfigs.port}`,
+            audience: `http://localhost:${appConfigs.port}`,
+            expiresIn: "7d"
+        },
+
+    },
+    "production": {
+        secret: process.env.JWT_SECRET,
+        options: {
+            issuer: process.env.JWT_ISSUER,
+            audience: process.env.JWT_AUDIENCE,
+            expiresIn: process.env.JWT_EXPIRES_IN
+        }
+    }
+}
+
+export default configs[process.env.NODE_ENV] || configs[defaultEnvironment];
